0138cdf2453bba9a5fc903493f20b9e3605ac734
Zaneh Williams <zanehmw@gmail.com>
2016-08-11 14:25:00 -0400

    Renders basic HTML

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello World!")
+  res.sendFile(__dirname + "/public/index.html")
 });
 
 app.listen(3001, function(){
