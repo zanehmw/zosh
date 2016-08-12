c1df5e8e86ca6b5bb89295e0a6dcc4d5f310621b
Joshua <Joshua.Nezianya@gmail.com>
2016-08-12 13:25:12 -0400

    Added basic angular controllers

--- public/js/app.js
+++ public/js/app.js
@@ -5,20 +5,42 @@
   .module("zosh", [
     "ui.router"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("productsIndexController", productsIndexCtrl)
+  .controller("productsShowController", productsShowCtrl);
   Router.inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
   function Router($stateProvider, $locationProvider, $urlRouterProvider){
     $locationProvider.html5Mode(true);
     $stateProvider
-    .state("main", {
-      url: "/",
-      template: "<h2>Major Key Alert</h2>"
+    .state("productsIndex", {
+      url:          "/",
+      templateUrl:     "/html/products-index.html",
+      controller:   "productsIndexController",
+      controllerAs: "pIndexVM"
     })
-    .state("test", {
-      url: "/test",
-      template: "<h2>Major Test Alert</h2>"
+    .state("productsShow", {
+      url: "/products/:name",
+      templateUrl: "/html/products-show.html",
+      controller: "productsShowController",
+      controllerAs: "pShowVM"
+
     });
     $urlRouterProvider.otherwise("/");
 
+  }
+  function productsIndexCtrl(){
+    var vm    = this;
+    vm.products   = [
+
+      {name: "Product1"},
+      {name: "Product2"},
+      {name: "Product3"}
+
+  ];
+  }
+  productsShowCtrl.$inject = ["$stateParams"];
+  function productsShowCtrl($stateParams){
+    var vm    = this;
+    vm.product  = $stateParams;
   }
 })();
